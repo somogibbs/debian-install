176a633c57b9d4ee7f1e8e9a14c3c91f7cbfca2a
somogibbs <somogibbs@gmail.com>
2025-05-07 12:18:03 -0500

    Update Steam-install.sh

--- Steam-install.sh
+++ Steam-install.sh
@@ -2,7 +2,5 @@
 #Make sure to enable "contrib" repositories
 
 sudo dpkg --add-architecture i386 &&
-
 sudo apt update &&
-
 sudo apt install libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386 steam-installer
