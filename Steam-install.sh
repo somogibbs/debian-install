03199f2893f8225f786e9d8b68d8daf98f77b473
somogibbs <somogibbs@gmail.com>
2025-05-12 09:39:00 -0500

    Update Steam-install.sh

--- Steam-install.sh
+++ Steam-install.sh
@@ -1,6 +1,13 @@
 !#/bin/bash
-#Make sure to enable "contrib" repositories
+# Enable contrib and non-free repos
+sudo apt-add-repository contrib
+sudo apt-add-repository non-free
 
-sudo dpkg --add-architecture i386 &&
-sudo apt update &&
+# Enable Multi-Arch
+sudo dpkg --add-architecture i386 
+
+# Update packages 
+sudo apt update && sudo apt upgrade -y
+
+# Install drivers and installer
 sudo apt install libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386 steam-installer
