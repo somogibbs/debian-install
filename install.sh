ec97a29357fb86254de3de2c6fccc48ab769ef6d
somogibbs <somogibbs@gmail.com>
2025-05-13 03:26:08 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 !#/bin/bash
 
 #Install Gnome components
-sudo apt -y install  gnome-core gnome-tweaks gnome-shell-extension-manager gnome-shell-extension-dashtodock libproxy1-plugin-networkmanager network-manager-gnome file-roller  gnome-software-plugin-flatpak gnome-extra-icons
+sudo apt -y install  gnome-core gnome-tweaks gnome-shell-extension-manager gnome-shell-extension-dashtodock libproxy1-plugin-networkmanager network-manager-gnome file-roller gnome-software-plugin-flatpak gnome-extra-icons
 
 #Install APT programs
-sudo apt -y install libavcodec-extra ffmpeg mpv qbittorent timeshift tldr neofetch openssh-server synaptic curl wget nala lshw stacer cpu-x btop lm-sensors preload gamemode xfce4-terminal libreoffice-writer firefox-esr blender mixxx pcsxr pcsx2 dolphin-emu yuzu obsidian-icon-theme fonts-recommended unzip
+sudo apt -y install libavcodec-extra ffmpeg mpv qbittorent timeshift tldr neofetch openssh-server synaptic curl wget nala lshw stacer cpu-x btop lm-sensors preload gamemode xfce4-terminal libreoffice-writer firefox-esr blender mixxx pcsxr pcsx2 dolphin-emu yuzu obsidian-icon-theme fonts-recommended variety
 
 #Install Flatpak and Flathub repository
 sudo apt -y install flatpak &&
