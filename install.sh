375bcfcc17f517a709b81b6692fcbc3e8300efa0
somogibbs <somogibbs@gmail.com>
2025-07-01 17:54:38 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 !#/bin/bash
 
 #Install Gnome components
-sudo apt -y install  gnome-core gnome-tweaks gnome-shell-extension-manager gnome-shell-extension-dashtodock libproxy1-plugin-networkmanager network-manager-gnome file-roller gnome-software-plugin-flatpak gnome-extra-icons
+sudo apt -y install  gnome-core gnome-tweaks gnome-shell-extension-manager gnome-shell-extension-dashtodock libproxy1-plugin-networkmanager network-manager-gnome file-roller gnome-software-plugin-flatpak gnome-extra-icons gnome-shell-extension-tiling-assistant gnome-shell-extension-blur-my-shell gnome-shell-extension-dashtodock
 
 #Install APT programs
-sudo apt -y install libavcodec-extra ffmpeg mpv qbittorent timeshift tldr neofetch openssh-server synaptic curl wget nala lshw stacer cpu-x btop lm-sensors preload gamemode xfce4-terminal libreoffice-writer firefox-esr blender mixxx pcsxr pcsx2 dolphin-emu yuzu obsidian-icon-theme fonts-recommended variety kiwix
+sudo apt -y install libavcodec-extra ffmpeg mpv qbittorent timeshift tldr neofetch openssh-server synaptic curl wget nala lshw stacer cpu-x btop lm-sensors preload gamemode xfce4-terminal libreoffice-writer firefox-esr mixxx yuzu obsidian-icon-theme fonts-recommended variety kiwix
 
 #Install Flatpak and Flathub repository
 sudo apt -y install flatpak &&
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 #Install Flatpak programs
-sudo flatpak install flathub tv.plex.PlexDesktop com.github.tchx84.Flatseal io.mango3d.LycheeSlicer it.mijorus.gearlever -y
+sudo flatpak install flathub tv.plex.PlexDesktop com.github.tchx84.Flatseal it.mijorus.gearlever -y
 
 #Install CoolerControl
 curl -1sLf \
@@ -30,22 +30,10 @@ wget --content-disposition linux-lowlatency.net/latest-appimage
 #Get Bitwarden .Deb package
 wget "https://vault.bitwarden.com/download/?app=desktop&platform=linux&variant=deb" -O bitwarden.deb
 
-#Install Floorp browser
-curl -fsSL https://ppa.floorp.app/KEY.gpg | sudo gpg --dearmor -o /usr/share/keyrings/Floorp.gpg
-sudo curl -sS --compressed -o /etc/apt/sources.list.d/Floorp.list 'https://ppa.floorp.app/Floorp.list'
-sudo apt update
-sudo apt install floorp
-
 #Setup UFW
 sudo apt -y install ufw &&
 sudo ufw allow ssh &&
 sudo ufw enable
-
-#Configure wifi - reboot
-sudo apt purge ifupdown -y && 
-sudo systemctl enable NetworkManager &&
-sudo systemctl start NetworkManager &&
-sudo shutdown -r now
 
 #Update TLDR
 tldr --update
@@ -53,3 +41,8 @@ tldr --update
 #Install Fallout bootloader theme
 wget -O - https://github.com/shvchk/fallout-grub-theme/raw/master/install.sh | bash
 
+#Configure wifi - reboot
+sudo apt purge ifupdown -y && 
+sudo systemctl enable NetworkManager &&
+sudo systemctl start NetworkManager &&
+sudo shutdown -r now
