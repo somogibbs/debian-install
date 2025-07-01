fa582b0384f09fcd801577c22d308423281fe78c
somogibbs <somogibbs@gmail.com>
2025-07-01 18:08:34 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@
 sudo apt -y install  gnome-core gnome-tweaks gnome-shell-extension-manager gnome-shell-extension-dashtodock libproxy1-plugin-networkmanager network-manager-gnome file-roller gnome-software-plugin-flatpak gnome-extra-icons gnome-shell-extension-tiling-assistant gnome-shell-extension-blur-my-shell gnome-shell-extension-dashtodock
 
 #Install APT programs
-sudo apt -y install libavcodec-extra ffmpeg mpv qbittorent timeshift tldr neofetch openssh-server synaptic curl wget nala lshw stacer cpu-x btop lm-sensors preload gamemode xfce4-terminal libreoffice-writer firefox-esr mixxx yuzu obsidian-icon-theme fonts-recommended variety kiwix
+sudo apt -y install libavcodec-extra ffmpeg mpv qbittorent timeshift tldr neofetch openssh-server synaptic curl wget lshw stacer cpu-x btop lm-sensors preload gamemode xfce4-terminal libreoffice-writer firefox-esr mixxx yuzu obsidian-icon-theme fonts-recommended variety kiwix
 
 #Install Flatpak and Flathub repository
 sudo apt -y install flatpak &&
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 #Install Flatpak programs
-sudo flatpak install flathub tv.plex.PlexDesktop com.github.tchx84.Flatseal it.mijorus.gearlever -y
+sudo flatpak install flathub tv.plex.PlexDesktop com.github.tchx84.Flatseal it.mijorus.gearlever org.feichtmeier.Musicpod -y
 
 #Install CoolerControl
 curl -1sLf \
@@ -23,9 +23,6 @@ sudo systemctl enable --now coolercontrold
 #Install Speedtest
 curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash &&
 sudo apt -y install speedtest
-
-#Get RPCS3 AppImage
-wget --content-disposition linux-lowlatency.net/latest-appimage 
 
 #Get Bitwarden .Deb package
 wget "https://vault.bitwarden.com/download/?app=desktop&platform=linux&variant=deb" -O bitwarden.deb
